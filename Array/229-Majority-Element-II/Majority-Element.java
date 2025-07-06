f7f270dccef54fa9524e7b4d37437aecdbb9b409
Krish Vora <krishvora22@gmail.com>
2025-07-06 23:48:21 +0530

    Time: 2127  ms (5.07%) | Memory: 52.54  MB (70.28%) - LeetSync

---
+++ Array/229-Majority-Element-II/Majority-Element.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> result = new ArrayList<>();
+
+        int n = nums.length;
+        int threshold = n / 3;
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > threshold) {
+                    result.add(nums[i - 1]);
+                }
+                count = 1;
+            }
+        }
+        if (count > threshold) {
+            result.add(nums[n - 1]);
+        }
+
+        return result;
+    }
+}
