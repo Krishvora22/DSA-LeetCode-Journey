7f7d0f9ae50a76b0e93233027a239d2b9265b2ed
Krishvora22 <krishvora22@gmail.com>
2025-09-08 21:26:35 +0530

    LCS problem

---
+++ DP/Longest-Comman-SubString/LCS.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public int longestCommonSubstr(String s1, String s2) {
+        // code here
+        int m=s1.length();
+        int n=s2.length();
+        int ans=0;
+        
+        int dp[][]=new int[m+1][n+1];
+        
+        for(int i=0;i<m+1;i++){
+            dp[i][0]=0;
+        }
+        
+        for(int j=0;j<n+1;j++){
+            dp[0][j]=0;
+        }
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ans = Math.max(ans, dp[i][j]);
+                } else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
